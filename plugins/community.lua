4b556609d54a3351b19d5bcdbee3c4d8b9876e71
Pavel Pavlov <kx355@mail.ru>
2023-06-28 10:14:07 +0300

    add some motion plugs

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,8 +2,15 @@ return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  -- availaule plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- action with surrounding
+  { import = "astrocommunity.motion.nvim-surround" },
+  -- smart motion with w,e,b on camelCase and meanless points
+  { import = "astrocommunity.motion.nvim-spider" },
+  -- sneak like motion with s/S and double symbol
+  { import = "astrocommunity.motion.leap-nvim" },
+  --
 }
